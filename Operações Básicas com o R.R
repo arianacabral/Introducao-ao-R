5f20696e9f3b7000f420887243f0700971787eca
arianacabral <arianacabral57@gmail.com>
2021-09-15 13:56:53 -0300

    Operações Básicas com o R

---
+++ Operações Básicas com o R.R
@@ -0,0 +1,90 @@
+# Operações Básicas com o R -----------------------------------------------
+
+## Operações Aritméticas no R
+
+# Adição
+
+242 + 11
+
+# Subtração 
+
+242 - 11
+
+# Multiplicação
+
+242 * 11
+
+# Divisão
+
+242 / 11
+
+# Quociente da divisão
+
+242 %/% 10
+
+# Resto da divisão
+
+242 %% 10
+
+# Exponenciação
+
+2^8
+
+## Operações Relacionais no R
+
+# Maior que
+
+5 < 10
+
+# Menor que 
+
+12 > 10
+
+# Maior ou igual a  
+
+5 >= 5
+
+# Menor ou igual a 
+
+10 <= 12
+
+# Igualdade
+
+10 == 10
+
+# Diferença
+
+5 != 10
+
+## Operações Lógicas no R
+
+# Operação NOT
+
+!(TRUE)
+
+!(4 > 20)
+
+# Operação AND elemento a elemento
+
+(1==1) & (0 < 2)
+
+(TRUE) & (FALSE)
+
+# Operação OR elemento a elemento
+
+(1 != 10) | (TRUE)
+
+(1 >= 10) | (0 > 0)
+
+## Atribuição
+
+# Atribuição à direita
+
+x <- 23
+y <<- 23
+
+# Atribuição à esquerda
+
+23 -> w
+23 ->> z
+
